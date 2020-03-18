9ada0f639fc389a07a21d409d545a4e535e046ef
tarr <tarr.mail@yandex.ru>
2020-03-18 18:05:36 +0300

    done

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined || matrix[0] == undefined) return [];
+  let result = '';
+  for (let i = 0; i < matrix.length; i++) {
+    if ((i + 1) % 2 == 0) {
+      result += String(matrix[i].reverse());
+    } else {
+      result += String(matrix[i]);
+    }
+    if (i != matrix.length - 1) result += ',';
+  }
+  return result.split(',');
 }
